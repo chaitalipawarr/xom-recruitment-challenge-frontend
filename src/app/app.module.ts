461382b5db97b9c6eb514cb335af31f4186260a0
Chaitali <rautchaitali19@gmail.com>
2023-01-22 02:56:15 +0100

    chore: upgrade angular to v15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppComponent } from './app.component';
 import { MockServerInterceptor } from './mock-server/mock-server.interceptor';
